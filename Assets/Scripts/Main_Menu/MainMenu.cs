2dda467f06233b8425bd68cb6378b3a14eeeeb34
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2023-05-12 22:41:44 +0300

    Added settings menu

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsMenu;
+    public GameObject titleMenu;
+
     public void LoadSingleplayerGame()
     {
         SceneManager.LoadScene("Singleplayer");
@@ -13,5 +16,17 @@ public class MainMenu : MonoBehaviour
     public void LoadMultiplayerGame()
     {
         SceneManager.LoadScene("COOP");
+    }
+
+    public void HideTitleMenu()
+    {
+        settingsMenu.SetActive(true);
+        titleMenu.SetActive(false);
+    }
+
+    public void HidePauseMenu()
+    {
+        settingsMenu.SetActive(false);
+        titleMenu.SetActive(true);
     }
 }
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer _audioMixer;
+
+    public TMPro.TMP_Dropdown _resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        _resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.value = currentResolutionIndex;
+        _resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        _audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
